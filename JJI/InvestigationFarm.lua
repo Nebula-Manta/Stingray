9aa54932569ee0669033f904750d4b67a0e5442e
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-12 00:54:40 +0000

    Delete JJI/InvestigationFarm.lua

--- JJI/InvestigationFarm.lua
+++
@@ -1,7 +0,0 @@
-repeat
-    task.wait()
-until game:IsLoaded()
-
--- Maintenance --
-setclipboard("https://discord.gg/ja8hEmmX")
-game.Players.LocalPlayer:Kick("Script is in maintenance!!!\n Discord server copied to clipboard for details (updated):\nCheck #important")
