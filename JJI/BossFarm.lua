22679fde36f39da26ce54017e6480703492212e2
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-22 17:02:17 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -1,22 +1,32 @@
 
+
+if not getgenv().Key then
+    getgenv().Key = readfile("Stingray_Key.txt")
+else
+    writefile("Stingray_Key.txt",getgenv().Key)
+end
+
 local Script = [[
     print("Post request failure")
 ]]
+
 local S,E = pcall(function()
     Script = request({
-    Url = "http://stingray-digital.online/script/jji",
-    Headers = {
-        ['Content-Type'] = 'application/json'
-    },
-    Body = game:GetService("HttpService"):JSONEncode({
-        key = tostring(getgenv().Key),
-        hwid = game:GetService("RbxAnalyticsService"):GetClientId(),
-        username = game:GetService("Players").LocalPlayer.Name
-    }),
-    Method = "POST"
-}).Body
+        Url = "http://stingray-digital.online/script/jji",
+        Headers = {
+            ['Content-Type'] = 'application/json'
+        },
+        Body = game:GetService("HttpService"):JSONEncode({
+            key = tostring(getgenv().Key),
+            hwid = game:GetService("RbxAnalyticsService"):GetClientId(),
+            username = game:GetService("Players").LocalPlayer.Name
+        }),
+        Method = "POST"
+    }).Body
 end)
+
 task.wait(1)
+
 if S then
     if #Script <= 200 then
         print(Script)
