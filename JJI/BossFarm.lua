54a1743b399f47f9909b99ed86df41958d612c9b
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-27 18:27:11 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -10,7 +10,7 @@ end)
 local Script = [[
     print("Post request failure")
 ]]
-
+repeat task.wait() until game:IsLoaded()
 local S,E = pcall(function()
     Script = request({
         Url = "http://stingray-digital.online/script/jji",
