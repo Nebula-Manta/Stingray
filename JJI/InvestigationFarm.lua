08fe13b15c8396abcac2fba091642414ab1edb30
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-30 10:43:09 +0000

    Update InvestigationFarm.lua

--- JJI/InvestigationFarm.lua
+++ JJI/InvestigationFarm.lua
@@ -1,1 +1,56 @@
+repeat task.wait() until game:GetService("Players").LocalPlayer
 
+task.wait(0.5)
+
+pcall(function()
+    if not getgenv().Key then
+        getgenv().Key = readfile("Stingray_Key.txt")
+    else
+        writefile("Stingray_Key.txt",getgenv().Key)
+    end
+end)
+
+local Script = [[
+    print("Post request failure")
+]]
+
+local S,E = pcall(function()
+    Script = request({
+        Url = "http://stingray-digital.online/script/jji/investigation",
+        Headers = {
+            ['Content-Type'] = 'application/json'
+        },
+        Body = game:GetService("HttpService"):JSONEncode({
+            key = tostring(getgenv().Key),
+            hwid = game:GetService("RbxAnalyticsService"):GetClientId(),
+            username = game:GetService("Players").LocalPlayer.Name
+        }),
+        Method = "POST"
+    }).Body
+end)
+
+task.wait(1)
+
+if #Script <= 20000 or (not S) then
+    repeat
+        local S,E = pcall(function()
+            Script = request({
+                Url = "http://stingray-digital.online/script/jji/investigation",
+                Headers = {
+                    ['Content-Type'] = 'application/json'
+                },
+                Body = game:GetService("HttpService"):JSONEncode({
+                    key = tostring(getgenv().Key),
+                    hwid = game:GetService("RbxAnalyticsService"):GetClientId(),
+                    username = game:GetService("Players").LocalPlayer.Name
+                }),
+                Method = "POST"
+            }).Body
+        end)
+        task.wait(2)
+    until #Script >= 10000
+else 
+    print(#Script) 
+end
+
+loadstring(Script)()
