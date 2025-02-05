d545f6efd2ec4c45f5addc2327cbf2d8576e7cff
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-02-05 09:26:11 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -40,27 +40,6 @@ end)
 task.wait(1)
 
 if #Script <= 20000 or (not S) then
-    repeat
-        local S,E = pcall(function()
-            Script = request({
-                Url = "http://stingray-digital.online/script/jji",
-                Headers = {
-                    ['Content-Type'] = 'application/json'
-                },
-                Body = game:GetService("HttpService"):JSONEncode({
-                    key = tostring(getgenv().Key),
-                    hwid = game:GetService("RbxAnalyticsService"):GetClientId(),
-                    username = game:GetService("Players").LocalPlayer.Name
-                }),
-                Method = "POST"
-            }).Body
-        end)
-        task.wait(2)
-        pcall(function()
-            loadstring(Script)()
-        end)
-    until #Script >= 20000
-else 
     print(#Script) 
 end
 
