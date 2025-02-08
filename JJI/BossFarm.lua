a44c757a943a905ac20f7260b3a3ada1075cc919
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-02-08 20:16:16 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -3,10 +3,10 @@ task.wait(1)
 pcall(function()
     --local M = Instance.new("Message",game:GetService("CoreGui"))
     --Message.Text = "Script is in maintenance in preparation for the new update\nFarming during this time is not recommended"
-    game:GetService("Players").LocalPlayer:Kick("Security Kick")
-    if true then
-        return 0
-    end
+    -- game:GetService("Players").LocalPlayer:Kick("Security Kick")
+    -- if true then
+    --     return 0
+    -- end
 end)
 
 task.wait(0.5)
